f653cd61d85b8d9ebf0dbb912a42dfee6d13eb4c
Caleb Elliott <caleb123elliott@gmail.com>
2022-05-16 14:52:47 -0600

    init / mostly completed

---
+++ main.js
@@ -0,0 +1,25 @@
+let home = "home"
+let away = "away"
+let scores = {
+  home: 0,
+  away: 0
+}
+
+function incrementScore(team, points) {
+  if (team == "home") {
+    scores.home += points
+    console.log("home score is " + scores.home);
+  }
+  else {
+    scores.away += points
+    console.log("Away score is " + scores.away);
+  }
+  drawScores()
+}
+
+function drawScores() {
+  document.getElementById("home-score").innerHTML = "  " + scores.home
+  document.getElementById("away-score").innerHTML = "  " + scores.away
+}
+
+drawScores()
